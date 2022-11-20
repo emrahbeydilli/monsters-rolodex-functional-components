b92efb27e18fea96a93a265796599a0d3de6061a
emrahbeydilli <emrahbeydilli@gmail.com>
2022-11-20 23:07:20 +0300

    finishing the project

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,14 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.style.css';
-
-
-class CardList extends Component {
-
-    render() {
-        const { monsters } = this.props;
-        return (
-            <div className='card-list'>
-                {
-                    monsters.map((monster) => {
-                        const {id} = monster;
-                        return <Card monster={monster} key={id}/>
-                    })
-                }
-            </div>
-        )
+const CardList = ({ monsters }) => (
+    <div className='card-list'>
+    {
+        monsters.map((monster) => {
+            const { id } = monster;
+            return <Card monster={monster} key={id} />
+        })
     }
-
-}
+</div>
+)
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,18 @@
-import { Component } from 'react';
 import './card.style.css'
 
-class Card extends Component {
-    render() {
-        const { id, name, email } = this.props.monster;
-        return (
-            <div className='card-container' >
-                <img
-                    src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                    alt={`monster ${name}`}
-                />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        );
-    }
+const Card = ({ monster }) => {
+    const { id, name, email } = monster;
+    return (
+        <div className='card-container' >
+            <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
 }
+
 
 export default Card;
